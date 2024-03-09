d364c56a78c3a632709ea766bc8b2fcadeafbd88
hainkiwanki <kevinr.be@gmail.com>
2024-03-09 20:51:03 +0100

    init commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { useQuasar } from 'quasar';
+
+const $q = useQuasar();
+
+function onButtonClick() {
+    $q.dark.toggle();
+}
+</script>
+
+<template>
+    <button type="button" @click="onButtonClick">Button</button>
+</template>
+
+<style scoped></style>
---
+++ src/main.ts
@@ -0,0 +1,18 @@
+import { createApp } from 'vue';
+import { Quasar } from 'quasar';
+import App from './App.vue';
+
+// Import icon libraries
+import '@quasar/extras/material-icons/material-icons.css';
+
+// Import Quasar css
+import 'quasar/src/css/index.sass';
+
+const app = createApp(App);
+app.use(Quasar, {
+    plugins: {},
+    config: {
+        dark: true,
+    },
+});
+app.mount('#app');
